90aa56a18f6e067d22110e9ce85255c69d6b868e
SrPicinini <bulaxu@gmail.com>
2021-10-10 01:02:04 -0400

    make it better

--- main.js
+++ main.js
@@ -1,45 +1,113 @@
 const venom = require('venom-bot');
 const fs = require('fs');
 const mime = require('mime-types');
+const venomOptions = require('./venom-options.js')
 
 venom
-  .create({
-        folderNameToken: 'tokens', //folder name when saving tokens
-        mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
-        headless: true, // Headless chrome
-        devtools: false, // Open devtools by default
-        useChrome: false, // If false will use Chromium instance
-        debug: false, // Opens a debug session
-        logQR: true, // Logs QR automatically in terminal
-        browserWS: '', // If u want to use browserWSEndpoint
-        browserArgs: [], //Original parameters  ---Parameters to be added into the chrome browser instance
-        puppeteerOptions: {'args' : [
-                            '--no-sandbox',
-                            '--disable-setuid-sandbox'
-        ]}, // Will be passed to puppeteer.launch
-        disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
-        disableWelcome: true, // Will disable the welcoming message which appears in the beginning
-        updatesLog: true, // Logs info updates automatically in terminal
-        autoClose: 1200000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
-    createPathFileToken: true //creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
-    })
+  .create(venomOptions)
   .then((client) => start(client));
 
 function start(client) {
-client.onMessage( async (message) => {
-
-
-    if (message.isMedia === true || message.body === '/sticker') {
+    client.onMessage( async (message) => {
+        if (message.body == '/menu'){
+            client.sendText('Ainda sou um Bot_Humilde tenho os seguintes comandos\n/marcar')
+        }
+        try{
             
-        const buffer = await client.decryptFile(message)
-        const fileName = `file.${mime.extension(message.mimetype)}`;
-        await fs.writeFile(fileName, buffer, (err) => {
+            if (message.body.includes('/marcar')){
+
+                msg = message.body
+                msg = msg.replace('/marcar', ' ')
+
+                marcados = []
+                adms_info = []
+
+                var adms = await client.getGroupAdmins(message.from);
+                var membros = await client.getGroupMembers(message.from);
+
+                membros.forEach(element => {
+                    marcados.push(element.id.user)
+                });
+
+                adms.forEach(element => {
+                    adms_info.push(element._serialized)
+                });
+
+                console.log(message.sender.id)
+                console.log(adms_info)
+
+                if (adms_info.includes(message.sender.id)){
+
+                    console.log(membros.pushname)
+
+                    client.sendMentioned(
+                        message.from,
+                        msg,
+                        marcados
+                    )
+                }
+            
+            }
+        }catch(err){'erro zé'}
+
+
+        try {
+
+            if (message.body.includes('.enviar')){
+
+                msg = message.body
+                msg = msg.replace('.enviar', ' ')
+
+                const chats = await client.getAllChats()
+                chats.forEach(date => {
+                    if (date.id.server === 'g.us'){
+                        console.log(msg)
+                        client.sendText(date.id._serialized, msg)
+                    }
+                })
+            }
+        }catch (error) {}
+
+        try {
+
+            if (message.body.includes('https://chat.whatsapp.com/')) {
+                console.log('tentandf')
+                client.joinGroup(message.matchedText).then(d => {console.log('certo')}).catch(d => {console.log('errado')})
+            }
+        }catch (error) {}
+        
+        
+
+        /*if (message.body == '/btv' || message.body == '/bentevi'){
+            client.sendImageAsSticker(message.from, 'btv.jpg')
+            client.sendVoice(message.from, 'btv.mp3')
+        }
+        
+
+        if (message.body == '/bailão' || message.body == '/bailao'){
+            client.sendImageAsStickerGif(message.from, 'yoshi.gif')
+            client.sendVoice(message.from, 'yoshi.mp3')
+        }
+
+        if (message.body == '/peep' || message.body == '/sad'){
+            client.sendImageAsStickerGif(message.from, 'sad.gif')
+            client.sendVoice(message.from, 'sad.mp3')
+        }*/
+
+        if (message.isMedia === true || message.isMMS === true) {
+            
+            const buffer = await client.decryptFile(message)
+            const fileName = `file.${mime.extension(message.mimetype)}`;
+            await fs.writeFile(fileName, buffer, (err) => {
+
             if (fileName === 'file.jpeg'){
 
                 console.log(message.sender.verifiedName + ' Enviou uma Foto, Criando Sticker...')
                 client.sendImageAsSticker(message.from, './file.jpeg')
+            
             } 
-        })
-    }
-})
+            });
+        }
+        
+    })
 }
