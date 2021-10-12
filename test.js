9336f58850d65eb3423e7fa801c6c294975b1163
SrPicinini <bulaxu@gmail.com>
2021-10-12 03:40:15 -0400

    make it better

--- test.js
+++ test.js
@@ -1,24 +1,10 @@
 const venom = require('venom-bot');
 const venomOptions = require('./venom-options.js')
-const axios = require('axios');
 const cotacao = require('./cotacao');
 
 venom
   .create(venomOptions)
   .then((client) => start(client));
-
-const buttons = [
-{
-    "buttonText": {
-    "displayText": "Sim"
-    }
-    },
-{
-    "buttonText": {
-    "displayText": "Não"
-    }
-    }
-]
 
 function start(client) {
 
